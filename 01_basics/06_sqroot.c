b370f81dd2e86311ab591a9e37a5854e791367f3
Prince Chauhan <chauhantanish4342@gmail.com>
2026-08-06 21:52:52 +0530

    Add square root calculation program in C

---
+++ 01_basics/06_sqroot.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+    float num;
+    printf("Enter a number\n");
+    scanf("%f",&num);
+    printf("Square root of %.2f is %.2f",num, sqrt(num));
+    return 0;
+}
